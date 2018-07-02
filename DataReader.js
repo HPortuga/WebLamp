30f9e847c04ec7a83ad3b07ddcafb39ef36e6b34
Matheus Soares <mathsoares1@gmail.com>
2018-07-02 16:07:01 -0400

    Criando projeto

---
+++ DataReader.js
@@ -0,0 +1,56 @@
+class DataReader {
+	constructor(event) {
+		var selectedFile = event.target.files[0];
+	    var reader = new FileReader();
+
+	    var that = this;
+	    reader.onload = function(event) {
+		    var contents = event.target.result.split("\n");
+			that.setContents(contents);	
+	    };
+
+	    reader.readAsText(selectedFile);
+	}
+
+	setContents(contents) {
+		this.contents = contents;
+		this.matriz = constroiMatriz(contents, 0, 0);
+	}
+}
+
+// Constrói uma matiz[][] a partir do conteúdo de um ficheiro ou uma matriz[][] vazia
+function constroiMatriz(contents, numLinhas, numDimensoes) {
+	if (contents == null) {
+		var matrix = new Array(numLinhas);
+		for (var i = 0; i < numLinhas; i++)
+			matrix[i] = new Array(numDimensoes);
+
+	} else {
+		var matrix = {
+			ids: [],
+			labels: [],
+			numLinhas: parseInt(contents[1]),
+			numDimensoes: parseInt(contents[2])
+		};
+
+		var dados = new Array(matrix.numLinhas);
+
+		for (var i = 0; i < matrix.numLinhas; i++) {
+			dados[i] = new Array(matrix.numDimensoes);
+		}
+
+		for (var i = 0; i < matrix.numLinhas; i++) {
+			var linha = contents[i+4].split(";");
+			matrix.ids[i] = parseInt(linha[0]);
+			for (var j = 0; j < matrix.numDimensoes; j++) {
+				dados[i][j] = parseFloat(linha[j+1]);
+			}
+			matrix.labels[i] = linha[j+1];
+		}
+
+		matrix.data = dados;
+	}
+
+	
+	return matrix;
+}
---
+++ Workflow.js
@@ -0,0 +1,123 @@
+var cy = cytoscape({
+  container: document.querySelector('#workflow'),
+
+  boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'content': 'data(name)',
+        'text-valign': 'center',
+        'color': 'white',
+        'text-outline-width': 2,
+        'background-color': '#999',
+        'text-outline-color': '#999'
+      })
+    .selector('node > node')
+      .css({
+        'background-color': '#d1ab10',
+        'width': 9,
+        'height': 9,
+      })
+    .selector('.ghost')
+      .css({
+        'visibility': 'hidden'
+      })
+    .selector('edge')
+      .css({
+        'curve-style': 'bezier',
+        'target-arrow-shape': 'triangle',
+        'target-arrow-color': '#ccc',
+        'line-color': '#ccc',
+        'width': 1
+      })
+    .selector(':selected')
+      .css({
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      })
+    .selector('.faded')
+      .css({
+        'opacity': 0.25,
+        'text-opacity': 0
+      }),
+
+  layout: {
+    name: 'grid', 
+    padding: 10
+  }
+});
+
+
+// WorkflowManager //
+
+// Add Data Reader
+var data;
+var nodeIds = 0;
+function onFileSelected(event) {
+  data = new DataReader(event); 
+}
+
+var btn = document.getElementById("addReader");
+btn.onclick = function() {
+  var nodeDataReader = {
+    nome: document.getElementById("readerName").value,
+    nSaidas: document.getElementById("nSaidasReader").value,
+    data: data
+  };
+
+  // [0] = parent node; [>0] = child nodes
+  var parentNode = [{
+    group: "nodes",
+    data: {
+      id: nodeIds++,
+      name: nodeDataReader.nome,
+      output: nodeDataReader.data.matriz
+    }
+  }];
+
+  // Parent size is set by its child's positions,
+  // so create ghost node to fix parent position
+  var ghostNode = {
+    data: {
+      id: nodeIds++,
+      parent: parentNode[0].data.id
+    },
+    classes: 'ghost'
+  };
+  parentNode.push(ghostNode);
+
+  // Set child node
+  var nodeSpacement = 0;
+  var nodeHeight = 33;
+  while (nodeDataReader.nSaidas-- > 0) {
+    var childNode = {
+      group: "nodes",
+      data: {
+        id: nodeIds++,
+        parent: parentNode[0].data.id
+      },
+      renderedPosition: {
+        x: nodeSpacement,
+        y: nodeHeight
+        }
+      };
+
+      nodeSpacement += 25;      
+      parentNode.push(childNode);
+  }
+   
+  cy.add(parentNode);
+
+  console.log(parentNode);
+
+  // lock child nodes
+  for (var i = 1; i < parentNode.length; i++) 
+    cy.$("#" + parentNode[i].data.id).ungrabify();
+  
+  // Stop the page from refreshing after btn click
+  return false;
+}
